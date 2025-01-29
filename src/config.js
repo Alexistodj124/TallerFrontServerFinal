57a42b1c28b3d87a902cdac0e4d9b43385094666
Alexistodj124 <alexistodj@gmail.com>
2025-01-29 17:50:44 -0600

    server api

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 const config = {
-    API_BASE_URL: "http://127.0.0.1:5000", // Change this URL when needed
+    //  API_BASE_URL: "http://127.0.0.1:5000", // Change this URL when needed
     
-    // API_BASE_URL: "http://3.140.209.59:8000", // Change this URL when needed
+    API_BASE_URL: "http://3.140.209.59:8000", // Change this URL when needed
 };
 
 export default config;
